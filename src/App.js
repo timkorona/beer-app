e003f66dc9617b8145a14510c314d389ea1928f7
tkorona-oh <tim.korona@ohiohealth.com>
2021-11-23 21:08:41 -0500

    commi beer app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchBeers = () => {
+    fetch("https://api.punkapi.com/v2/beers?per_page=80")
+      .then((response) => response.json())
+      .then((data) => {
+        data.sort ((a, b) => b.abv - a.abv);
+        setData(data);
+      });
+  };
+
+  function DryHopAlert(inHopsArray) {
+    for (let i=0; i < inHopsArray.length; i++) {
+      if (inHopsArray[i].add.toLowerCase() === "dry hop") {
+        return <Alert severity="info">Dry Hopped</Alert>;
+      }
+    };
+
+    return;
+  };
+
+  function LactoseAlert(inString) {
+    if (inString != null && inString.toLowerCase().includes("lactose")) {
+      return (<Alert severity="warning">Contains Lactose</Alert>);
+    };
+
+    return;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{marginTop: 16, marginBottom: 32}}>
+        <Button variant="contained" onClick={fetchBeers}>Get List of Beers</Button>
+      </div>
+      <div>
+        <TableContainer sx={{maxWidth: '80%', mx: 'auto'}} component={Paper}>
+          <Table size="small" aria-label="list of beers">
+            <TableHead>
+              <TableRow sx={{backgroundColor: 'lightGray'}}>
+                <TableCell align="center" sx={{width: '5%'}}>Image</TableCell>
+                <TableCell align="center" sx={{width: '15%'}}>Name of Beer</TableCell>
+                <TableCell align="center" sx={{width: '20%'}}>Beer Tagline</TableCell>
+                <TableCell align="center" sx={{width: '50%'}}>Description</TableCell>
+                <TableCell align="center" sx={{width: '5%'}}>ABV</TableCell>
+                <TableCell align="center" sx={{width: '5%'}}>IBU</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell align="center">
+                    <img alt="" src={row.image_url} style={{height: 100}}></img>
+                  </TableCell>
+                  <TableCell align="left">
+                    {row.name}
+                    {LactoseAlert(row.method.twist)}
+                    {DryHopAlert(row.ingredients.hops)
+                  }</TableCell>
+                  <TableCell align="left">{row.tagline}</TableCell>
+                  <TableCell align="left">{row.description}</TableCell>
+                  <TableCell align="center">{row.abv}</TableCell>
+                  <TableCell align="center">{row.ibu}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+      </TableContainer>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,133 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import fetchMock from 'fetch-mock';
+import Button from '@mui/material/Button';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const beerList = [
+  {
+    "id": 1,
+    "name": "Buzz",
+    "tagline": "A Real Bitter Experience.",
+    "description": "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.",
+    "image_url": "https://images.punkapi.com/v2/keg.png",
+    "abv": 4.5,
+    "ibu": 60,
+    "method": {
+      "twist": "Do the lactose twist."
+    },
+    "ingredients": {
+      "hops": [
+        {
+          "add": "start",
+        },
+        {
+          "add": "start",
+        }
+      ]
+    }
+  },
+  {
+    "id": 2,
+    "name": "Buzz2",
+    "tagline": "A Real Bitter Experience.",
+    "description": "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.",
+    "image_url": "https://images.punkapi.com/v2/keg.png",
+    "abv": 4.5,
+    "ibu": 60,
+    "method": {
+      "twist": "No L-word here."
+    },
+    "ingredients": {
+      "hops": [
+        {
+          "add": "start",
+        },
+        {
+          "add": "dry hop",
+        }
+      ]
+    }
+  }
+];
+fetchMock.mock('https://api.punkapi.com/v2/beers?per_page=80', {
+  data: beerList,
+  status: 200
 });
+
+afterEach(() => {
+  fetchMock.restore();
+})
+
+test('renders button for retrieving list of beers', () => {
+  render(<App />);
+  const buttonElement = screen.getByRole('button', { name: 'Get List of Beers' });
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('renders table of beers', () => {
+  render(<App />);
+  const tableContainerElement = screen.getByRole('table', { name: 'list of beers' });
+  expect(tableContainerElement).toBeInTheDocument();
+});
+
+test('renders Image column', () => {
+  render(<App />);
+  const imageColumnElement = screen.getByRole('columnheader', { name: 'Image' });
+  expect(imageColumnElement).toBeInTheDocument();
+});
+
+test('renders Name of Beer column', () => {
+  render(<App />);
+  const nameColumnElement = screen.getByRole('columnheader', { name: 'Name of Beer' });
+  expect(nameColumnElement).toBeInTheDocument();
+});
+
+test('renders Beer Tagline column', () => {
+  render(<App />);
+  const taglineContainerElement = screen.getByRole('columnheader', { name: 'Beer Tagline' });
+  expect(taglineContainerElement).toBeInTheDocument();
+});
+
+test('renders Description column', () => {
+  render(<App />);
+  const descriptionColumnElement = screen.getByRole('columnheader', { name: 'Description' });
+  expect(descriptionColumnElement).toBeInTheDocument();
+});
+
+test('renders ABV column', () => {
+  render(<App />);
+  const abvColulmnElement = screen.getByRole('columnheader', { name: 'ABV' });
+  expect(abvColulmnElement).toBeInTheDocument();
+});
+
+test('renders IBU column', () => {
+  render(<App />);
+  const ibuColumnElement = screen.getByRole('columnheader', { name: 'IBU' });
+  expect(ibuColumnElement).toBeInTheDocument();
+});
+
+//I am befuddled as to why using line 115 does not work but line 116 does.
+//Line 116 hits jest.fn(), while line 115 appears to hit the actual fetchBeers function.
+//Line 115 gives a message stating absence of the jest.fn() for the fetch results in the
+//actual fetch being hit but jest.fn() is there, what am I missing?
+test('call fetchBeers when Get List of Beers button clicked', () => {
+  const fetchBeers = jest.fn();
+  render(<App />);
+  // render(<Button variant="contained" onClick={fetchBeers}>Get List of Beers</Button>);
+  const button = screen.getByRole('button', { name: 'Get List of Beers' });
+  expect(button).toBeInTheDocument();
+  fireEvent.click(button);
+  expect(fetchBeers).toHaveBeenCalled();
+});
+
+// test('displays alert warning if beer contains lactose', () => {
+// });
+
+// test('does NOT display alert warning if beer does NOT contain lactose', () => {
+// });
+
+// test('displays info alert if one of the ingredients is "dry hop"', () => {
+// });
+
+// test('does NOT display info alert if no "dry hop" ingredient', () => {
+// });
